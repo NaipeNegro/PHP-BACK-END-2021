36155388c0c5031ade2e3600d0d35bc704141a77
Negro <ramirosuarezpaz@gmail.com>
2021-11-13 17:20:01 -0300

    publicacion

--- index.php
+++ index.php
@@ -8,9 +8,14 @@
     <title>PHP</title>
 </head>
 <body>
-    <h1>Hola</h1>
+
     <?php
-        echo '<p>Esto viene de PHP</body>p>';
+        echo '    <h1>Hola</h1>
+        <h2> Mi nombre es Ramiro</h2>
+        <h2>Mi apellido Suárez Paz</h2>
+        <h2>Tengo 33 años</h2>
+        <h3>Jugar online es mi hobbie</h3>
+        <h3>Visual Studio es mi editor de código preferido</h3>';
     ?>
 </body>
 </html>
